bf26c70cafe402c727b76dca9b02f8429494a15c
Artem <pandos.jimmyrose@gmail.com>
2015-06-08 17:49:54 +0300

    azzzian

--- azzzian/js/main.js
+++ azzzian/js/main.js
@@ -42,13 +42,13 @@ $(document).ready(function(){
 		}
 		event.stopPropagation();
 	});
-
+	/*
 	$(".owl-carousel").owlCarousel({
 	    navigation : false, // Show next and prev buttons
 	    slideSpeed : 300,
 	    paginationSpeed : 400,
 	    singleItem:true
-	});
+	});*/
 });
 
 
