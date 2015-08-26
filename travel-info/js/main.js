405afab64f79e0d0c896ee74666c71c6358c33db
Artem <pandos.jimmyrose@gmail.com>
2015-08-26 14:48:25 +0300

    travel-info

--- travel-info/js/main.js
+++ travel-info/js/main.js
@@ -3,31 +3,19 @@ $(document).ready(function(){
 	/* TOP MENU SLIDE*/
 	(function($){
 		var top_mnu_enter_checker = false;
-		$(".rubricator-shower").on("mouseenter", function(){
+		$(".rubricator-shower").on("click", function(){
 			if (!$(this).hasClass("active")){
 				$(this).addClass("active")
 				$(".top-mnu").slideDown(400);
 			}
-			top_mnu_enter_checker = true;
-			console.log('rubricator-shower mouseenter');
-		});
-
-		$(".rubricator-shower").mouseout(function(event) {
-			if(!$(event.relatedTarget).hasClass("top-mnu")){
-				$(this).removeClass("active")
+			else{
+				$(this).removeClass("active");
 				$(".top-mnu").slideUp(400);
 			}
-			console.log('rubricator-shower mouseout');
+			top_mnu_enter_checker = true;
+			console.log('rubricator-shower click');
 		});
 
-		$(".top-mnu").on("mouseleave",function(event){
-			if(!$(event.relatedTarget).hasClass("rubricator-shower")){
-				$(".rubricator-shower").removeClass("active")
-				$(this).slideUp(400);
-			}
-			top_mnu_enter_checker = false;
-			console.log('top-mnu mouseleave');
-		});
 
 		$(".header-select").selectpicker({
 	      size: 4
@@ -360,6 +348,103 @@ $(document).ready(function(){
 		});
 	}
 	
+	/**/
+
+	/*EVENT CALENDAR*/
+	if($(".event_calendar").length){
+		var Event = function(id, text) {
+			this.id = id;
+		    this.text = text;
+		};
+		var Shedule =function(event_id,time,text,company){
+			this.event_id = event_id;
+			this.time = time;
+			this.text = text;
+			this.company = company;
+		}
+		var events = {};
+		var shedules = {};
+		events[new Date("08/14/2015")] = new Event(1, "Valentines Day");
+		events[new Date("08/18/2015")] = new Event(2, "Payday");
+		events[new Date("09/18/2015")] = new Event(2, "Payday");
+
+		shedules[1] = [
+			new Shedule(1,"09:00","Ужин в отеле","Georgian airways"),
+			new Shedule(1,"09:40","Выставка в музее","Georgian airways")
+		];
+		shedules[2] = [
+			new Shedule(1,"09:00","Ужин в отеле","Georgian airways"),
+			new Shedule(1,"09:40","Выставка в музее","Georgian airways")
+		];
+		shedules[2] = [
+			new Shedule(1,"09:00","Ужин в отеле","Georgian airways"),
+			new Shedule(1,"09:40","Выставка в музее","Georgian airways"),
+			new Shedule(1,"09:40","Поездка","Georgian airways")
+		];
+		$.datepicker._updateDatepicker_original = $.datepicker._updateDatepicker;
+	    $.datepicker._updateDatepicker = function(inst) {
+	        $.datepicker._updateDatepicker_original(inst);
+	        var afterShow = this._get(inst, 'afterShow');
+	        if (afterShow)
+	            afterShow.apply((inst.input ? inst.input[0] : null));  // trigger custom callback
+	    }
+
+	    var event_ids = [];
+	    var rus_monthes = ["января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря"];
+		$(".event_calendar").datepicker({
+			firstDay: 1,
+			monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'], 
+			monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн', 
+			'Июл','Авг','Сен','Окт','Ноя','Дек'], 
+			dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'], 
+			dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'], 
+			dayNamesMin: ['Вс.','Пн.','Вт.','Ср.','Чт.','Пт.','Сб.'],
+			showOtherMonths: true,
+			beforeShowDay: function(date) {
+		        var event = events[date];
+		        if (event) {
+		        	unic_class = "event"+event.id;
+		        	event_ids.push(event.id);
+		            return [true, "highlight "+unic_class, event.text];
+		        }
+		        else {
+		            return [true, '', ''];
+		        }
+		    },
+		    onSelect: function(dateText,obj){
+		    	var currentDate = new Date(dateText);
+		    	var event = events[currentDate];
+		        if($(".event_calendar.deployed").length){
+		        	var day = currentDate.getDate();
+		        	var month = rus_monthes[currentDate.getMonth()];
+		        	var shedules_html = '<h2 class="event_shedules_title">События на '+day+' '+month+'</h2>';
+		        	shedules[event.id].forEach(function(item, i, arr) {
+					  shedules_html += '<div class="event_shedule">';
+					  shedules_html += '<div class="event_shedule_time">'+item.time+'</div>';
+					  shedules_html += '<div class="event_shedule_text">'+item.text+'</div>';
+					  shedules_html += '<div class="event_shedule_company">'+item.company+'</div> ';
+					  shedules_html += '</div>';
+					});
+					$(".event_shedules").fadeOut(200,function(){
+						$(".event_shedules").html(shedules_html);
+						$(".event_shedules").fadeIn(200);
+					});
+					
+
+		        }
+		        
+		    },
+		    afterShow:function(){
+		    	i = 0;
+		    	$(".event_calendar .highlight").each(function(){
+		    		id = event_ids[i];
+		    		$(this).find("a").attr("data-counter",shedules[id].length);
+		    		i++;
+		    	});
+		    }
+		});
+	
+	}
 	/**/
 });
 
