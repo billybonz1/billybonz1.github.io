697573f9263fd324f052d0dbd4b49fa1b7236c15
Artem <artem21kalinichenko@gmail.com>
2016-01-25 13:54:52 +0200

    adaptive1

--- gavrilenko/adaptive1/js/common.js
+++ gavrilenko/adaptive1/js/common.js
@@ -9,6 +9,24 @@ $(document).ready(function(){
         theme = $(this).attr('class');
         $("body").addClass(theme);
     });
+    $('.up').mouseover( function(){
+        $( this ).animate({opacity: 0.65},100);
+    }).mouseout( function(){
+        $( this ).animate({opacity: 1},100);
+    }).click( function(){
+        window.scroll(0 ,0);
+        return false;
+    });
+
+    $(window).scroll(function(){
+        if ( $(document).scrollTop() > 0 ) {
+            $('.up').fadeIn('fast');
+        } else {
+            $('.up').fadeOut('fast');
+        }
+    });
+
+
     video();
 });
 $(window).resize(function(){
